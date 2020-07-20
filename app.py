b4cdce5c1a819dc47fb3750f96c92c393d155cfd
k018c1472 <k018c1472@it-neec.jp>
2020-07-20 14:16:56 +0900

    5.1.http_method

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    list = {'apple' : 'リンゴ','orange' : 'オレンジ','lemon' : 'レモン'}
-    return render_template('index.html', list=list)
+    return render_template('index.html')
 
+@app.route('/send',methods=['POST'])
+def send():
+    msg = request.form.get('msg')
+    return render_template('receive.html', message=msg)
 if __name__=='__main__':
     app.debug = True
     app.run()
