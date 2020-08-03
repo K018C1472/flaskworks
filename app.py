0177fdfc2d138cddf2bea0153e955c105263d1d6
k018c1472 <k018c1472@it-neec.jp>
2020-08-03 17:22:06 +0900

    8.2.database(select)

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import mysql.connector as db
+
+db_param = {
+    'user' : 'mysql',
+    'host' : 'localhost',
+    'password' : '',
+    'database' : 'db1'
+}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './static/uploads/'
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+   conn = db.connect(**db_param)
+   cur = conn.cursor()
+   stmt = 'SELECT * FROM books'
+   cur.execute(stmt)
+   rows = cur.fetchall()
+   cur.close()
+   conn.close()
+   return render_template('index.html',books=rows)
 
-@app.route('/send',methods=['POST'])
-def send():
-     img_file = request.files['img_file']
-     if img_file and allowed_file(img_file.filename):
-         img_file.save(app.config['UPLOAD_FOLDER'] + img_file.filename)
-         return '<p>画像' + img_file.filename + 'を送信しました</p>'
-     else:
-         return '<p>許可されていない拡張子です。</p>'
 if __name__=='__main__':
     app.debug = True
     app.run()
